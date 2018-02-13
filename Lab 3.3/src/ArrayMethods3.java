f4855a83f90f26fd9660c1c091fb23d30f8bd9d2
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-02-13 08:51:07 -0500

    first

---
+++ Lab 3.3/src/ArrayMethods3.java
@@ -0,0 +1,22 @@
+
+public class ArrayMethods3 {
+	
+	public static String[] mergeSort(String[] list) 
+	{
+		return list;
+		
+	}
+	
+	public static int partition(int[] list1, int front, int back) 
+	{
+		return back;
+		
+	}
+	
+	public static void quickSort(int[] list1, int front, int back)
+	{
+		
+	}
+	
+	
+}

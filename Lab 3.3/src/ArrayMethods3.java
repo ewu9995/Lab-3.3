4e9c29e504a6a3ce3f4604d4fc7e30023ddff794
Author <PC@Dell>
2018-02-24 22:42:55 -0500

    null

--- Lab 3.3/src/ArrayMethods3.java
+++ Lab 3.3/src/ArrayMethods3.java
@@ -1,31 +1,30 @@
 import java.util.Arrays;
-
+/*
+ * Evan Wu
+ * Period 2 AP CSA
+ * Lab 3.3
+ */
 public class ArrayMethods3 {
 	
 	//starter code
 		public static void main(String[] args) {
-			int[] list1 = {4,1,6,0,8,9,2,3,};
+			int[] list1 = {4,7,1,3,2,6,5,8,9,0};
 			String[] list = {"ferwe","aerwe","yeeee", "zzzzzz", "aaaaaa"};
 			
 			long start = System.nanoTime();
 			String[] mergeResult = mergeSort(list);
 			long end = System.nanoTime();
 			long time = end - start;
-			System.out.println("Merge test took: " + time + " nanoseconds");
+			System.out.println("Merge Sort test took: " + time + " nanoseconds");
 			System.out.println(Arrays.toString(mergeResult));
 			
 			start = System.nanoTime();
-			int pivotFinalPos = partition(list1, 1, 1);
 			end = System.nanoTime();
 			time = end - start;
-			System.out.println("Partition test took: " + time + " nanoseconds");
-			System.out.println("Final Pivot Position: " + pivotFinalPos);
+			quickSort(list1, 0, list1.length-1);
+			System.out.println("Quick Sort test took: " + time + " nanoseconds");
 			System.out.println(Arrays.toString(list1));
 		}
-		 
-	
-
-
 
 	public static String[] mergeSort(String[] list) 
 	{
@@ -34,13 +33,17 @@ public class ArrayMethods3 {
 			return list;
 		//Recursive step
 		
-		// left is index to to 1/2
+		//two arrays left and right since merge sort is splitting up arrays by half then combining it back together sorted
+		//left is 0 to to 1/2
 		//right is 1/2 to end
-		String[] left = Arrays.copyOfRange(list, 0, list.length/2 );
-		String[] right = Arrays.copyOfRange(list, list.length/2, list.length); 
+		String[] left = Arrays.copyOfRange(list, 0, list.length/2);
+		String[] right = Arrays.copyOfRange(list, list.length/2 , list.length); 
 		return(merge(mergeSort(left), mergeSort(right)));
 		
 	}
+	
+	//Sourced from: https://stackoverflow.com/questions/5958169/how-to-merge-two-sorted-arrays-into-a-sorted-array
+
 	public static String[] merge(String[] list1 , String[] list2) {
 
 			String [] combinedArray = new String[list1.length + list2.length]; //new array must be length of both arrays combined
@@ -49,62 +52,68 @@ public class ArrayMethods3 {
 			int z=0; //new array (combinedArray)
 			  while (x < list1.length && y < list2.length) //both arrays must be less than their intended length (any longer with break the code)
 		        {
-		            if (list1[x].compareTo(list2[y]) < 0) //compares the values in the 2 arrays and see which is smaller
+		            if (list1[x].compareTo(list2[y]) <= 0) //compares the values in the 2 arrays and see which is smaller
 		            {
 		               combinedArray[z] = list1[x]; //since the value in list1 is smaller in this case, list1[x] becomes part of the sorted array before list2[y]
-		                x++;
+		                x++; //x will count up as the values are placed into the merged array
+		                z++; //z is the merged array index so it counts up as it fills in its slots
 		            }
-		            else
+		            else  
 		            {
 		            	combinedArray[z] = list2[y]; //since list2[y] is smaller, it is placed before list1[x]
-		                y++; 
+		                y++; //y counts up as values are placed into merged array
+		                z++;
 		            }
-		            z++;
+		            
 		        	}
 			  		
 			  		//loops through the arrays to find values
 			  		if (x < list1.length)
 			  		{
-			        combinedArray[z] = list1[x];
-			        x++; z++;
+			  			combinedArray[z] = list1[x];
+			  			x++;
+			  			z++;
 			  		}
 
 			  		if (y < list2.length)
 			  		{
-			        combinedArray[z] = list2[y];
-			        y++; z++;
+			  			combinedArray[z] = list2[y];
+			  			y++; 
+			  			z++;
 			  		}
 
-			    return combinedArray; //returns the combined sorted array 
+			    return combinedArray; //returns the merged sorted array 
 		}
 		
 	
-	
+	// https://tekmarathon.com/2013/09/17/understanding-quicksort-algorithm/
+	// checks to see if values are greater than pivot
+	// if value is less than pivot then it is left of it (right if greater)
 	public static int partition(int[] list1, int front, int back) 
 	{
-		int pivot = list1.length-1/2;
-		int pivotLocation = 0;
-		back = list1.length-1;
-		front = 0;
-		for(int i = 1; i <list1.length; i++ ) {
-			if (list1[i] <= pivot) 
-			 {
-				swap(list1, i, i-1); 
-				pivotLocation = i;
-			 }
-			 
-			else if(list1[i] > pivot && back > i)
-			 {
-			
-				swap(list1, i, back);
-				i--;
-				back--;
-			 }
-			 
+		int pivot = list1[front];
+		while (front < back) {
+			while (list1[front] < pivot) {
+				//since the value is less than the pivot then nothing happens and it counts up
+				//until a grater value is found
+				front++;
 			}
-			return pivotLocation; 	
+			while (list1[back] > pivot) {
+				//if the value is greater than the pivot nothing happens and it counts down
+				//until smaller value is found
+				back--;
+			}
+			if(front <= back) {
+			//value swaps as conditions are met
+			swap(list1, front, back);
+			
+			//everything continues on
+			front++;
+			back--;
+			}
 		}
-		
+		return front; //pivot index is returned
+    }
 	
 	//swaps values
 	public static void swap(int []arr, int index1, int index2) {
@@ -112,15 +121,14 @@ public class ArrayMethods3 {
 		arr[index1] = arr[index2];
 		arr[index2] = temp;
 	}
-
-
 	
+
 	public static void quickSort(int[] list1, int front, int back)
 	{
 		if(back>front) {
 			int pivotPos = partition(list1, front, back);
-			quickSort(list1, front, pivotPos-1);
-			quickSort(list1, pivotPos+1, back);
+			quickSort(list1, front, pivotPos-1); //values before pivot
+			quickSort(list1, pivotPos, back); //values after pivot
 		}
 	}
 	
